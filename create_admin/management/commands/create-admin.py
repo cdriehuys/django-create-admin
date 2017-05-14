d78669c4bee87091a4272130cabc51001beaa158
Chathan Driehuys <cdriehuys@gmail.com>
2017-05-14 16:13:56 -0400

    Modify username and password to be named arguments.

--- create_admin/management/commands/create-admin.py
+++ create_admin/management/commands/create-admin.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
@@ -12,8 +12,19 @@ class Command(BaseCommand):
         """
         Add available options to the parser.
         """
-        parser.add_argument('username', type=str)
-        parser.add_argument('password', type=str)
+        parser.add_argument(
+            '-u',
+            '--username',
+            action='store',
+            help='The username to create the admin with',
+            type=str)
+        parser.add_argument(
+            '-p',
+            '--password',
+            action='store',
+            help=('The password to give the admin. Only has an effect when '
+                  '--username is specified.'),
+            type=str)
 
     def handle(*args, **options):
         """
@@ -21,6 +32,13 @@ class Command(BaseCommand):
         """
         username = options.get('username')
         password = options.get('password')
+
+        if not username:
+            raise CommandError('--username and --password are required.')
+
+        if not password:
+            raise CommandError('--password is required when a username is '
+                               'given.')
 
         user = get_user_model().objects.create_user(
             username=username,
--- create_admin/tests/test_create_admin.py
+++ create_admin/tests/test_create_admin.py
@@ -1,5 +1,7 @@
-from django.core.management import call_command
+from django.core.management import CommandError, call_command
 from django.utils.six import StringIO
+
+import pytest
 
 
 def test_basic_create(django_user_model):
@@ -8,10 +10,38 @@ def test_basic_create(django_user_model):
     and password.
     """
     out = StringIO()
-    call_command('create-admin', 'foo', 'bar', out=out)
+    call_command('create-admin', username='foo', password='bar', out=out)
 
     user = django_user_model.objects.get(username='foo')
 
     assert user.check_password('bar')
     assert user.is_staff
     assert user.is_superuser
+
+
+def test_no_args():
+    """
+    If no arguments are given, the script should throw an error giving
+    the required arguments.
+    """
+    out = StringIO()
+
+    with pytest.raises(CommandError) as excinfo:
+        call_command('create-admin', out=out)
+
+    assert '--username and --password are required' in str(excinfo.value)
+
+
+def test_username_no_password():
+    """
+    If a username is provided but a password isn't, an error should be
+    raised.
+    """
+    out = StringIO()
+
+    with pytest.raises(CommandError) as excinfo:
+        call_command('create-admin', username='foo', out=out)
+
+    expected = '--password is required when a username is given.'
+
+    assert expected in str(excinfo.value)
